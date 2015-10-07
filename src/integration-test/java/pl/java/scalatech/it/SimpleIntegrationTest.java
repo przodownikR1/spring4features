c1165f72b52dba9ad2ebe94f011009cb7cc4b8f5
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-07 12:36:30 +0200

    increment libs

--- src/integration-test/java/pl/java/scalatech/it/SimpleIntegrationTest.java
+++ src/integration-test/java/pl/java/scalatech/it/SimpleIntegrationTest.java
@@ -2,12 +2,13 @@ package pl.java.scalatech.it;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.fest.assertions.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
 @Slf4j
 public class SimpleIntegrationTest {
     @Test
-    public void shouldIntegrationTestWork(){
+    public void shouldIntegrationTestWork() {
         log.info("+++      integration test run");
         Assertions.assertThat(true);
     }
--- src/test/java/pl/java/scalatech/test/configurator/MyConfigTest.java
+++ src/test/java/pl/java/scalatech/test/configurator/MyConfigTest.java
@@ -2,7 +2,7 @@ package pl.java.scalatech.test.configurator;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.fest.assertions.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,21 +13,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import pl.java.scalatech.configuration.MyConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {MyConfig.class})
+@ContextConfiguration(classes = { MyConfig.class })
 @Slf4j
 public class MyConfigTest {
-  //http://www.intertech.com/Blog/spring-4-conditional-bean-configuration/
-    @Autowired 
+    //http://www.intertech.com/Blog/spring-4-conditional-bean-configuration/
+    @Autowired
     private String value;
     @Autowired
     private Environment env;
+
     @Test
-    public void shouldFindBean(){
+    public void shouldFindBean() {
         log.info("+++                 normal test");
-       if(env.getProperty("user.country").equals("PL")){
-        Assertions.assertThat(value).isNotEmpty();
-       }else{
-           Assertions.assertThat(value).isEqualTo("foreign");
-       }
+        if (env.getProperty("user.country").equals("PL")) {
+            Assertions.assertThat(value).isNotEmpty();
+        } else {
+            Assertions.assertThat(value).isEqualTo("foreign");
+        }
     }
 }
